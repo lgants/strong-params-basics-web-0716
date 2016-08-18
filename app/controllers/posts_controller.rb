83ad9ccc2071de1bbdd8da477134b5aac5f1c6ca
lgants <lgants@gmail.com>
2016-08-18 13:36:11 -0400

    strong params lab complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,18 +12,69 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params["post"])
+	  @post = Post.new(post_params(:title, :description))
 	  @post.save
 	  redirect_to post_path(@post)
 	end
 
+	# def create
+	#   @post = Post.new(post_params)
+	#   @post.save
+	#   redirect_to post_path(@post)
+	# end
+
+	# def create
+	#   @post = Post.new(params.require(:post).permit(:title, :description))
+	#   @post.save
+	#   redirect_to post_path(@post)
+	# end
+
+	# def create
+	#   @post = Post.new(params["post"])
+	#   @post.save
+	#   redirect_to post_path(@post)
+	# end
+
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(params["post"])
+	  @post.update(post_params(:title))
 	  redirect_to post_path(@post)
 	end
+
+	# def update
+	#   @post = Post.find(params[:id])
+	#   @post.update(post_params)
+	#   redirect_to post_path(@post)
+	# end
+
+	# def update
+	#   @post = Post.find(params[:id])
+	#   @post.update(params.require(:post).permit(:title))
+	#   redirect_to post_path(@post)
+	# end
+
+	# def update
+	#   @post = Post.find(params[:id])
+	#   @post.update(params["post"])
+	#   redirect_to post_path(@post)
+	# end
 
 	def edit
 	  @post = Post.find(params[:id])
 	end
+
+	private
+
+		# We pass the permitted fields in as *args;
+		# this keeps `post_params` pretty dry while
+		# still allowing slightly different behavior
+		# depending on the controller action
+		def post_params(*args)
+			#assumes nested params hash, with key of post, pointing to a hash
+		  params.require(:post).permit(*args)
+		end
+
+		# def post_params
+  	# 	params.require(:post).permit(:title, :description)
+		# end
 end
